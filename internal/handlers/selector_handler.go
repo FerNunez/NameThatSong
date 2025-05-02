f219f1cc7b6901797d80f660f6009eb41156ab0e
Fernando NUNEZ <fernunezmendoza@gmail.com>
2025-05-02 17:31:08 +0200

    wip: add select album handler

--- internal/handlers/selector_handler.go
+++ internal/handlers/selector_handler.go
@@ -3,65 +3,21 @@ package handlers
 import (
 	"fmt"
 	"net/http"
-	"sort"
 
 	"github.com/FerNunez/NameThatSong/internal/manager"
-	"github.com/FerNunez/NameThatSong/internal/spotify_api"
 	"github.com/FerNunez/NameThatSong/internal/templates"
 )
 
-type GetSearchArtists struct {
+type PostSelectAlbum struct {
 	gm *manager.GameManager
 }
 
-func NewGetSearchArtists(gm *manager.GameManager) *GetSearchArtists {
-	return &GetSearchArtists{gm}
+func NewPostSelectAlbum(gm *manager.GameManager) *PostSelectAlbum {
+	return &PostSelectAlbum{gm}
 }
 
-func (h *GetSearchArtists) ServeHttp(w http.ResponseWriter, r *http.Request) {
-	game, err := h.gm.GetGame(r.Context())
-	if err != nil {
-		fmt.Printf("error getting game : %v", err)
-		return
-	}
-	// Get query term
-	query := r.URL.Query().Get("search")
-	if query == "" {
-		// Return empty results component
-		component := templates.SearchResults([]spotify_api.ArtistData{})
-		component.Render(r.Context(), w)
-		return
-	}
+func (h *PostSelectAlbum) ServeHttp(w http.ResponseWriter, r *http.Request) {
 
-	artists, err := game.SearchArtists(query)
-	if err != nil || len(artists) == 0 {
-		// TODO: SEND ERROR TO REQUEST
-		component := templates.SearchResults([]spotify_api.ArtistData{})
-		component.Render(r.Context(), w)
-		return
-	}
-
-	// Sort by popularity
-	sort.Slice(artists, func(i, j int) bool {
-		return artists[i].Popularity > artists[j].Popularity
-	})
-
-	// Return results component
-	component := templates.SearchResults(artists)
-	component.Render(r.Context(), w)
-}
-
-////////////////////////////
-
-type GetArtistAlbums struct {
-	gm *manager.GameManager
-}
-
-func NewGetArtistAlbums(gm *manager.GameManager) *GetArtistAlbums {
-	return &GetArtistAlbums{gm}
-}
-
-func (h *GetArtistAlbums) ServeHttp(w http.ResponseWriter, r *http.Request) {
 	game, err := h.gm.GetGame(r.Context())
 	if err != nil {
 		fmt.Printf("error getting game : %v", err)
