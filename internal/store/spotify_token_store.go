d549364ec72949c7ef2cedca130cec16b59c8944
Fernando NUNEZ <fernunezmendoza@gmail.com>
2025-05-29 17:02:24 +0200

    add crytography to spotify token store

--- internal/store/spotify_token_store.go
+++ internal/store/spotify_token_store.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/FerNunez/NameThatSong/internal/crypto"
 	"github.com/FerNunez/NameThatSong/internal/store/database"
 	"github.com/google/uuid"
 )
@@ -26,18 +27,30 @@ type SpotifyTokenStore interface {
 
 // ////////////////////////////////////////////
 type SQLSpotifyTokenStore struct {
-	db *database.Queries
+	db        *database.Queries
+	encryptor *crypto.TokenEncryptor
 }
 
-func NewSQLSpotifyTokenStore(db *database.Queries) SpotifyTokenStore {
-	return &SQLSpotifyTokenStore{db}
+func NewSQLSpotifyTokenStore(db *database.Queries, encryptor *crypto.TokenEncryptor) SpotifyTokenStore {
+	return &SQLSpotifyTokenStore{db, encryptor}
 }
 
 func (s *SQLSpotifyTokenStore) Create(ctx context.Context, user_id uuid.UUID, refresh_token, access_token, token_type, scope string, expires_at time.Time) error {
 
-	_, err := s.db.CreateSpotifyToken(ctx, database.CreateSpotifyTokenParams{
-		RefreshToken: refresh_token,
-		AccessToken:  access_token,
+	// Encrypt tokens!
+	refresh_token_crypted, err := s.encryptor.Encrypt(refresh_token)
+	if err != nil {
+		return err
+	}
+	access_token_crypted, err := s.encryptor.Encrypt(access_token)
+	if err != nil {
+		return err
+	}
+
+	// Store data
+	_, err = s.db.CreateSpotifyToken(ctx, database.CreateSpotifyTokenParams{
+		RefreshToken: refresh_token_crypted,
+		AccessToken:  access_token_crypted,
 		TokenType:    token_type,
 		Scope:        scope,
 		ExpiresAt:    expires_at,
@@ -54,9 +67,18 @@ func (s *SQLSpotifyTokenStore) Get(ctx context.Context, user_id uuid.UUID) (Spot
 
 	fmt.Println("Getting Token:", dbSpotifyToken.AccessToken)
 
+	refresh_token_decrypted, err := s.encryptor.Decrypt(dbSpotifyToken.RefreshToken)
+	if err != nil {
+		return SpotifyToken{}, err
+	}
+	access_token_decrypted, err := s.encryptor.Decrypt(dbSpotifyToken.AccessToken)
+	if err != nil {
+		return SpotifyToken{}, err
+	}
+
 	return SpotifyToken{
-		RefreshToken: dbSpotifyToken.RefreshToken,
-		AccessToken:  dbSpotifyToken.AccessToken,
+		RefreshToken: refresh_token_decrypted,
+		AccessToken:  access_token_decrypted,
 		TokenType:    dbSpotifyToken.TokenType,
 		Scope:        dbSpotifyToken.Scope,
 		ExpiresAt:    dbSpotifyToken.ExpiresAt,
