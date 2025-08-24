02846187c3a1587b9de3974ab800c54ad1e95805
Fernando NUNEZ <fernunezmendoza@gmail.com>
2025-08-24 18:36:30 +0200

    remove unused templs

--- internal/api/handlers/modern_handler.go
+++ internal/api/handlers/modern_handler.go
@@ -3,9 +3,9 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/a-h/templ"
 	"github.com/FerNunez/NameThatSong/internal/api/middleware"
 	"github.com/FerNunez/NameThatSong/web/templates"
+	"github.com/a-h/templ"
 )
 
 type ModernHandler struct{}
@@ -19,7 +19,7 @@ func (h *ModernHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user, ok := middleware.GetUser(r.Context())
 	var templateUser *templates.User
 	var indexComponent templ.Component
-	
+
 	if ok && user != nil {
 		templateUser = &templates.User{
 			Name:      user.DisplayName,
@@ -27,15 +27,15 @@ func (h *ModernHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			AvatarURL: user.AvatarURL,
 		}
 		// Show the full modern interface for authenticated users
-		indexComponent = templates.ModernIndexPage()
+		indexComponent = templates.IndexPage()
 	} else {
 		// Show welcome page for unauthenticated users
 		indexComponent = templates.WelcomeIndexPage()
 	}
-	
+
 	// Wrap it in the modern layout with user data
-	component := templates.ModernLayoutWithUser(indexComponent, "NameThatSong - Modern Interface", templateUser)
-	
+	component := templates.LayoutWithUser(indexComponent, "NameThatSong", templateUser)
+
 	// Render the component
 	err := component.Render(r.Context(), w)
 	if err != nil {
