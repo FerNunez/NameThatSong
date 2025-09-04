b447412786a38a8a001908c949a1d954b7aadb54
Fernando NUNEZ <fernunezmendoza@gmail.com>
2025-09-04 13:53:12 +0200

    placed interface in same file for easy fzf

--- internal/services/cache/spotify_cache_redis.go
+++ internal/services/cache/spotify_cache_redis.go
@@ -13,6 +13,44 @@ import (
 	"github.com/FerNunez/NameThatSong/internal/pkg/utils"
 )
 
+type SpotifyCache interface {
+	// Basic entity cache operations (cache-only, no API calls)
+	GetTrack(trackId string) (m.TrackData, bool)
+	SetTrack(trackId string, track m.TrackData)
+	GetMultipleTracks(trackIDs []string) (map[string]m.TrackData, []string)
+	SetMultipleTracks(tracks map[string]m.TrackData)
+	GetAlbum(albumId string) (m.AlbumData, bool)
+	SetAlbum(albumId string, album m.AlbumData)
+	GetMultipleAlbums(albumIDs []string) (map[string]m.AlbumData, []string)
+	SetMultipleAlbums(albums map[string]m.AlbumData)
+	GetArtist(artistId string) (m.ArtistData, bool)
+	SetArtist(artistId string, artist m.ArtistData)
+	GetMultipleArtists(artistIDs []string) (map[string]m.ArtistData, []string)
+	SetMultipleArtists(artists map[string]m.ArtistData)
+	GetPlaylist(playlistId string) (m.PlaylistData, bool)
+	SetPlaylist(playlistId string, playlist m.PlaylistData)
+	GetMultiplePlaylists(artistIDs []string) (map[string]m.PlaylistData, []string)
+	SetMultiplePlaylists(artists map[string]m.PlaylistData)
+	GetPlaylistTracks(playlistId string) ([]string, bool)
+	SetPlaylistTracks(playlistId string, tracks []string)
+	GetPlaylistAlbums(playlistId string) ([]string, bool)
+	SetPlaylistAlbums(playlistId string, tracks []string)
+
+	// Search cache operations (cache-only, no API calls)
+	GetSearchTracks(query string) ([]m.TrackSearch, bool)
+	SetSearchTracks(query string, tracks []m.TrackSearch)
+	GetSearchAlbums(query string) ([]m.AlbumSearch, bool)
+	SetSearchAlbums(query string, albums []m.AlbumSearch)
+	GetSearchArtists(query string) ([]m.ArtistSearch, bool)
+	SetSearchArtists(query string, artists []m.ArtistSearch)
+	GetSearchPlaylists(query string) ([]m.PlaylistSearch, bool)
+	SetSearchPlaylists(query string, playlists []m.PlaylistSearch)
+
+	// OAuth state management
+	GetOAuthState(userID string) (string, bool)
+	SetOAuthState(userID, state string)
+}
+
 type RedisSpotifyCache struct {
 	client *redis.Client
 	ctx    context.Context
